aea83c856022f68453986b41daf0153a6ccd3016
Somnath Sarak <spsarak123@gmail.com>
2025-12-24 06:50:56 +0530

    Refactor DashboardPage to use PageFactory pattern for WebElement locators
    Refactor DashboardPage to use PageFactory for element locators.

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -2,105 +2,105 @@ package pages;
 
 import base.ExtentReportManager;
 import base.WebDriverUtils;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import java.util.List;
 
 /**
- * OrangeHRM Dashboard Page Object Model
+ * OrangeHRM Dashboard Page Object Model using PageFactory
  * Contains locators and methods for OrangeHRM Dashboard page
  */
 public class DashboardPage {
     private WebDriver driver;
 
-    // Dashboard Page Locators
-    private By dashboardTitle = By.xpath("//h6[text()='Dashboard']");
-    private By welcomeMessage = By.xpath("//p[contains(text(), 'Welcome')]");
-    private By sidebarMenu = By.className("oxd-sidebar-menu");
-    private By adminModule = By.xpath("//a[@href='/web/index.php/admin/viewAdminModule']");
-    private By pimModule = By.xpath("//a[@href='/web/index.php/pim/viewPimModule']");
-    private By leaveModule = By.xpath("//a[@href='/web/index.php/leave/viewLeaveModule']");
-    private By recruitmentModule = By.xpath("//a[@href='/web/index.php/recruitment/viewRecruitmentModule']");
-    private By userDropdown = By.xpath("//p[@class='oxd-userdropdown-name']");
-    private By logoutOption = By.xpath("//a[@href='/web/index.php/auth/logout']");
-    private By profileButton = By.xpath("//button[contains(text(), 'Profile')]");
-    private By dashboardWidgets = By.className("oxd-grid-item");
-
     public DashboardPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-    public boolean isDashboardDisplayed() {
-        ExtentReportManager.getExtentTest().info("Checking if Dashboard is displayed");
+    // Dashboard Page locators using @FindBy annotation
+    @FindBy(xpath = "//h6[text()='Dashboard']")
+    private WebElement dashboardTitle;
+
+    @FindBy(xpath = "//div[@class='oxd-sidepanel-body']")
+    private WebElement sidebarMenu;
+
+    @FindBy(xpath = "//div[@class='oxd-grid-item'][contains(.,'Employees on Leave Today')]")
+    private WebElement employeeOnLeaveWidget;
+
+    @FindBy(xpath = "//div[@class='oxd-grid-item'][contains(.,'Work Anniversaries')]")
+    private WebElement workAnniversaryWidget;
+
+    @FindBy(xpath = "//div[@class='oxd-grid-item'][contains(.,'Upcoming Holidays')]")
+    private WebElement upcomingHolidaysWidget;
+
+    @FindBy(xpath = "//button[@class='oxd-icon-button oxd-icon-button--solid-main' and @type='button']")
+    private WebElement userDropdown;
+
+    @FindBy(xpath = "//a[contains(text(), 'Logout')]")
+    private WebElement logoutOption;
+
+    @FindBy(xpath = "//div[@class='oxd-layout-context']//div[@class='oxd-grid-item']")
+    private List<WebElement> dashboardWidgets;
+
+    @FindBy(xpath = "//button[@class='oxd-icon-button oxd-icon-button--solid-main' and @title='View Profile']")
+    private WebElement profileButton;
+
+    public boolean isDashboardPageDisplayed() {
+        ExtentReportManager.getExtentTest().info("Checking if dashboard page is displayed");
         boolean isDisplayed = WebDriverUtils.isElementDisplayed(driver, dashboardTitle);
-        ExtentReportManager.getExtentTest().info("Dashboard displayed: " + isDisplayed);
+        ExtentReportManager.getExtentTest().info("Dashboard page displayed: " + isDisplayed);
         return isDisplayed;
-    }
-
-    public String getDashboardTitle() {
-        ExtentReportManager.getExtentTest().info("Getting dashboard title");
-        String title = WebDriverUtils.getText(driver, dashboardTitle);
-        ExtentReportManager.getExtentTest().info("Dashboard title: " + title);
-        return title;
-    }
-
-    public boolean isWelcomeMessageDisplayed() {
-        ExtentReportManager.getExtentTest().info("Checking if welcome message is displayed");
-        boolean isDisplayed = WebDriverUtils.isElementDisplayed(driver, welcomeMessage);
-        ExtentReportManager.getExtentTest().info("Welcome message displayed: " + isDisplayed);
-        return isDisplayed;
-    }
-
-    public void navigateToAdminModule() {
-        ExtentReportManager.getExtentTest().info("Navigating to Admin module");
-        WebDriverUtils.clickElement(driver, adminModule);
-        ExtentReportManager.getExtentTest().info("Admin module clicked");
-    }
-
-    public void navigateToPIMModule() {
-        ExtentReportManager.getExtentTest().info("Navigating to PIM module");
-        WebDriverUtils.clickElement(driver, pimModule);
-        ExtentReportManager.getExtentTest().info("PIM module clicked");
-    }
-
-    public void navigateToLeaveModule() {
-        ExtentReportManager.getExtentTest().info("Navigating to Leave module");
-        WebDriverUtils.clickElement(driver, leaveModule);
-        ExtentReportManager.getExtentTest().info("Leave module clicked");
-    }
-
-    public void navigateToRecruitmentModule() {
-        ExtentReportManager.getExtentTest().info("Navigating to Recruitment module");
-        WebDriverUtils.clickElement(driver, recruitmentModule);
-        ExtentReportManager.getExtentTest().info("Recruitment module clicked");
-    }
-
-    public void logout() {
-        ExtentReportManager.getExtentTest().info("Logging out from dashboard");
-        WebDriverUtils.clickElement(driver, userDropdown);
-        ExtentReportManager.getExtentTest().info("User dropdown clicked");
-        WebDriverUtils.clickElement(driver, logoutOption);
-        ExtentReportManager.getExtentTest().info("Logout option clicked");
     }
 
     public boolean isSidebarMenuDisplayed() {
         ExtentReportManager.getExtentTest().info("Checking if sidebar menu is displayed");
-        boolean isDisplayed = WebDriverUtils.isElementDisplayed(driver, sidebarMenu);
-        ExtentReportManager.getExtentTest().info("Sidebar menu displayed: " + isDisplayed);
-        return isDisplayed;
+        return WebDriverUtils.isElementDisplayed(driver, sidebarMenu);
+    }
+
+    public boolean isQuickLaunchDisplayed() {
+        ExtentReportManager.getExtentTest().info("Checking if quick launch is displayed");
+        return dashboardWidgets.size() > 0;
+    }
+
+    public boolean isEmployeeOnLeaveDisplayed() {
+        ExtentReportManager.getExtentTest().info("Checking if employee on leave widget is displayed");
+        return WebDriverUtils.isElementDisplayed(driver, employeeOnLeaveWidget);
+    }
+
+    public boolean isWorkAnniversaryDisplayed() {
+        ExtentReportManager.getExtentTest().info("Checking if work anniversary widget is displayed");
+        return WebDriverUtils.isElementDisplayed(driver, workAnniversaryWidget);
+    }
+
+    public void clickUserDropdown() {
+        ExtentReportManager.getExtentTest().info("Clicking user dropdown");
+        WebDriverUtils.clickElement(driver, userDropdown);
+        ExtentReportManager.getExtentTest().info("User dropdown opened");
+    }
+
+    public boolean isUserDropdownDisplayed() {
+        ExtentReportManager.getExtentTest().info("Checking if user dropdown is displayed");
+        return WebDriverUtils.isElementDisplayed(driver, logoutOption);
+    }
+
+    public void clickLogoutOption() {
+        ExtentReportManager.getExtentTest().info("Clicking logout option");
+        WebDriverUtils.clickElement(driver, logoutOption);
+        ExtentReportManager.getExtentTest().info("Logged out successfully");
     }
 
     public int getDashboardWidgetCount() {
         ExtentReportManager.getExtentTest().info("Getting dashboard widget count");
-        List<WebElement> widgets = driver.findElements(dashboardWidgets);
-        ExtentReportManager.getExtentTest().info("Widget count: " + widgets.size());
-        return widgets.size();
+        int count = dashboardWidgets.size();
+        ExtentReportManager.getExtentTest().info("Dashboard widgets count: " + count);
+        return count;
     }
 
-    public void clickProfileButton() {
-        ExtentReportManager.getExtentTest().info("Clicking profile button");
-        WebDriverUtils.clickElement(driver, profileButton);
-        ExtentReportManager.getExtentTest().info("Profile button clicked");
+    public boolean isProfileButtonDisplayed() {
+        ExtentReportManager.getExtentTest().info("Checking if profile button is displayed");
+        return WebDriverUtils.isElementDisplayed(driver, profileButton);
     }
 }
